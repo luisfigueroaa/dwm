788fe3ef0cad094ed469c67f94ce39eeb32d42c0
luis <luis_figueroa_morales@yahoo.com>
2022-07-05 11:30:36 -0500

    systray correction and windows with tags

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const unsigned int gappoh    = 10;       /* horiz outer gap between windo
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayonleft = 1;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
@@ -44,6 +44,11 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Pavucontrol",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Steam",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Steam",  NULL,       "Friends List",       1 << 8,       1,           -1 },
+	{ "Thunderbird",  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Thunderbird",  "Msgcompose",       NULL,       1 << 7,       1,           -1 },
+	{ "Virt-manager",  NULL,       "Gestor de máquinas virtuales",       1 << 6,       1,           -1 },
 };
 
 /* layout(s) */
