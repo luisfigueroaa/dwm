919e87fe2dc53bfe344a0065541ed5b2c8eca3d2
Luis Figueroa <luis_figueroa_morales@yahoo.com>
2022-12-25 20:52:48 -0500

    asian characters support

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:size=10:antialias=true:autohint=true", "Noto Sans Mono CJK KR:size=10"  };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
